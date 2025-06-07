c1a2d2ad78690729a4c81e50de30b046b70bf9c5
Priyank Shah <83683507+p1209s@users.noreply.github.com>
2025-06-07 12:16:21 -0400

    update public

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NavBar from "./components/NavBar.jsx";
 import Hero from "./sections/Hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Hero />
+      <ShowcaseSection />
     </>
   );
 }
