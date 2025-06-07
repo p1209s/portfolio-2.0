00c37a1da5324ffd31a3bf93445fff02ffcb0256
Priyank Shah <83683507+p1209s@users.noreply.github.com>
2025-06-07 17:38:00 -0400

    update public

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <ShowcaseSection />
       <LogoSection />
       <FeatureCards />
+      <ExperienceSection />
     </>
   );
 }
